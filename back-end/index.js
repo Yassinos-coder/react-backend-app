c62eafd53aeb4b60251ba5aa3f4f104feb05bca7
Yassinos <37705952+Yassinos-coder@users.noreply.github.com>
2022-11-19 01:11:32 +0100

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -7,7 +7,6 @@ const TaskManager = require('./APIs/tasksAPI')
 
 app.use(express.json())
 app.use(cors())
-app.use(UserRouter)
 app.listen(9000, ()=> console.log('Server is up and running on port 9000'))
 //mongoose.connect('mongodb+srv://zeus:1234@cluster0.z9ikdbp.mongodb.net/Todos', ()=> console.log('DB connection granted'))
 mongoose.connect('mongodb://127.0.0.1:27017/Todos', ()=> console.log('Database connection granted'))
